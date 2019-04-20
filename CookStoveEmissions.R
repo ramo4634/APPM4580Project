8d7aa216ce59a9b30114be0def38fb0fd5cc3cab
Rachel Moore <ramo4634@colorado.edu>
2019-04-20 17:38:22 -0600

    Try to read xlsx sheet
    Weird error

--- CookStoveEmissions.R
+++ CookStoveEmissions.R
@@ -1,3 +1,10 @@
 ################################################################
 # Final Project
 ################################################################
+library(pROC)
+library(dplyr)
+library(e1071)
+library(MASS)
+library(readxl)
+
+fires <- read_xls("P3_kitchen_area_air_quality_03_20_2019.xlsx")

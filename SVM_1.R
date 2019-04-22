b0da56e31abcd49c5a10c90fac9b5b20df2afd04
Kathryn Gray <kathryn.gray@colorado.edu>
2019-04-22 16:48:05 -0600

    Beginning SVM model
    Still needs work on parameter tuning

---
+++ SVM_1.R
@@ -0,0 +1,65 @@
+library(readxl)
+library(e1071)
+
+
+filename <- #FILENAME HERE
+dat<- read_excel(filename)
+
+y <- dat$TotalPM25ugcubicmeter_PT 
+feature_list = c("OCmassugcubicmeter","ECmassugcubicmeter","TCmassugcubicmeter","ECOCmassconc","Mean_bcor_1","Mean_bcor_2","Mean_temp","Mean_rh","Mean_CO","Mean_CO2","Mean_MCE","Var_bcor_1")#,"CoverageClass","season") 
+X <- dat[feature_list]
+#Replace NA values with mean - note this only works for numeric data
+for(i in colnames(X)){
+  print(mean(X[[i]], na.rm=TRUE))
+  X[[i]][is.na(X[[i]])] <- mean(X[[i]],na.rm=TRUE)
+}
+
+X$y <- y
+
+## 60% of the sample size for training 20% for testing and 20% for verification
+smp_size <- floor(0.6 * length(y))
+
+set.seed(42)
+train_ind <- sample(seq_len(length(y)), size = smp_size)
+
+X.train <- X[train_ind,]
+y.train <- y[train_ind]
+
+X.temp <- X[-train_ind,]
+y.temp <- y[-train_ind]
+
+smp_size <- floor(0.5*length(y.temp))
+test_ind <- sample(seq_len(length(y.temp)), size = smp_size)
+
+X.test <- X.temp[test_ind,]
+y.test <- y.temp[test_ind]
+X.ver <- X.temp[-test_ind,]
+y.test <- y.temp[-test_ind]
+
+
+
+fit <- svm(y~.,data=X.train,kernel='linear',cost=2)
+y.pred <- predict(fit, X.test)
+
+rmse <- function(error)
+{
+  sqrt(mean(error^2))
+}
+
+error <- fit$residuals  # same as data$Y - predictedY
+predictionRMSE <- rmse(error)   # 5.703778
+print(predictionRMSE)
+#Grid Search
+tuneResult <- tune(svm, y ~ .,  data = X.train, ranges = list(epsilon = seq(0,1,0.1), cost = 2^(2:9)))
+print(tuneResult)
+
+tunedModel <- tuneResult$best.model
+tunedModelY <- predict(tunedModel, X.test) 
+
+error <- y.test - tunedModelY  
+
+# this value can be different on your computer
+# because the tune method  randomly shuffles the data
+tunedModelRMSE <- rmse(error)  # 2.219642  
+
+

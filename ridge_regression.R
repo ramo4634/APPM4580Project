f92afe486af8a838a52c27e8bfef5d3f623d1f61
booksforever <kathryn.gray@colorado.edu>
2019-04-27 22:55:41 -0600

    Added ridge regression model

---
+++ ridge_regression.R
@@ -0,0 +1,69 @@
+################################################################################################
+## Ridge Regression
+################################################################################################
+library(readxl)
+library(glmnet)
+
+#========================================
+# Setting up the data
+#========================================
+
+filename <- "P3_kitchen_area_air_quality_03_20_2019.xlsx"
+dat<- read_excel(filename)
+
+y <- dat$TotalPM25ugcubicmeter_PT 
+feature_list = c("OCmassugcubicmeter","ECmassugcubicmeter","TCmassugcubicmeter","ECOCmassconc","Mean_bcor_1","Mean_bcor_2","Mean_temp","Mean_rh","Mean_CO","Mean_CO2","Mean_MCE","Var_bcor_1")#,"CoverageClass","season") 
+X <- dat[feature_list]
+#Replace NA values with mean - note this only works for numeric data
+for(i in colnames(X)){
+  X[[i]][is.na(X[[i]])] <- mean(X[[i]],na.rm=TRUE)
+}
+X$y <- y
+
+
+#========================================
+# Train/Test Split
+#========================================
+
+## 80% of the sample size for training 20% for testing
+smp_size <- floor(0.8 * length(y))
+
+
+set.seed(42)
+train_ind <- sample(seq_len(length(y)), size = smp_size)
+
+X.train <- X[train_ind,]
+y.train <- y[train_ind]
+
+X.test <- X[-train_ind,]
+y.test <- y[-train_ind]
+
+
+#========================================
+# Ridge Regression using glmnet
+#========================================
+
+X.train <- as.matrix(X.train)
+y.train <-as.matrix(y.train)
+X.test <- as.matrix(X.test)
+y.test <- as.matrix(y.test)
+
+lambdas <- 10^seq(3, -2, by = -.1)
+rr.test <- cv.glmnet(X.train,y.train,alpha=0,lambda=lambdas)
+lambda.opt <- rr.test$lambda.min
+plot(rr.test)
+
+rr.final <- glmnet(X.train,y.train,alpha=0,lambda=lambda.opt)
+
+rr.pred <- predict(rr.final,s=0.1,newx=as.matrix(X.test))
+
+mean( (mean(y.test) - y.test)^2 ) 
+mean( (rr.pred - y.test)^2 ) 
+rmse <- function(error)
+{
+  sqrt(mean(error^2))
+}
+
+rrRMSE = rmse(y.test-lasso.pred)
+print(rrRMSE)
+

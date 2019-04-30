085062f2d6575368801d703b51a99018ed6f5ae0
Rachel Moore <ramo4634@colorado.edu>
2019-04-29 22:01:32 -0600

    Adding Outlier Code
    Taking out outliers

--- CookStoveEmissions.R
+++ CookStoveEmissions.R
@@ -75,12 +75,12 @@ plot(bestFeatures)
 coef(bestFeatures,8)
 
 plot(y,dat$Mean_bcor_1)
-plot(y,log(dat$Mean_bcor_2)
+plot(y,log(dat$Mean_bcor_2))
 # ============================================================
 #  Fitting the model
 # ============================================================
  
-fit.lm <- lm(y~ECOCmassconc+Mean_bcor_1+Mean_bcor_2+Mean_temp+Mean_rh+Mean_CO+Mean_CO2+Mean_MCE,data=X.train)
+fit.lm <- lm(y~ECOCmassconc+Mean_bcor_1+Mean_bcor_2+Mean_temp+Mean_rh+Mean_CO+Mean_CO2+Mean_MCE,data=dat)
 fit.bcor1 <- lm(y~Mean_bcor_1,data=X.train)
 fit.bcor2 <- lm(y~Mean_bcor_2,data=X.train)
 
@@ -92,20 +92,17 @@ plot(fit.lm)
 plot(fit.lm, id.n = NULL)                 # no id's
 plot(fit.lm, id.n = 5, labels.id = NULL)  # 5 id numbers
 
-boxplot(X.train$ECOCmassconc,X.train$Mean_bcor_1,X.train$Mean_bcor_2,X.train$Mean_temp,X.train$Mean_rh,X.train$Mean_CO,X.train$Mean_CO2,X.train$Mean_MCE)
-boxplot(X.train$Mean_bcor_1,X.train$Mean_bcor_2)
-OutVals = boxplot(X.train$Mean_bcor_1)$out
-which(X.train$Mean_bcor_1 %in% OutVals)
-OutVals = boxplot(X.train$Mean_bcor_2)$out
-which(X.train$Mean_bcor_2 %in% OutVals)
-mybox <- boxplot(X.train$Mean_bcor_1,X.train$Mean_bcor_2)
+boxplot(dat$ECOCmassconc,dat$Mean_bcor_1,dat$Mean_bcor_2,dat$Mean_temp,dat$Mean_rh,dat$Mean_CO,dat$Mean_CO2,dat$Mean_MCE)
+boxplot(dat$Mean_bcor_1,dat$Mean_bcor_2)
+OutVals = boxplot(dat$Mean_bcor_1)$out
+which(dat$Mean_bcor_1 %in% OutVals)
+OutVals = boxplot(dat$Mean_bcor_2)$out
+which(dat$Mean_bcor_2 %in% OutVals)
+mybox <- boxplot(dat$Mean_bcor_1,dat$Mean_bcor_2)
 print(head(mybox))
-X.train[6,]
-X.train[12,]
-X.train[21,]
+dat[17,]
+dat[18,]
+dat[37,]
+dat = dat[c(-17,-18,-37),]
 #####
-X.train[8,]
-X.train[10,]
-X.train[7,]
-X.train[13,]
-X.train[19,]
+
